329d9b7835a05c262f3f55314bf9d59e5b256efb
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 11:48:51 -0700

    Update book.h

--- book.h
+++ book.h
@@ -5,7 +5,25 @@
 #include <set>
 #include <vector>
 #include <algorithm>
+#include "product.h"
+#include "util.h"
 
+class Book : public product {
+public:
+  std::set<std::string> keywords() const {
+    return parseStringToWords(name_);
+  }
 
+  std::string displayString() const {
+    return (name_ + "\nAuthor: " + 
+  }
+
+  void dump(std::ostream& os) const {
+    os << displayString();
+  }
+
+protected:
+  std::string isbn_;
+}
 
 #endif
