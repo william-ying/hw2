2e6f68ac96c320d6cf784a813ede65fd23d57e55
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 14:10:07 -0700

    Create mydatastore.h

---
+++ mydatastore.h
@@ -0,0 +1,18 @@
+#ifndef MYDATASTORE_H
+#define MYDATASTORE_H
+#include "datastore.h"
+
+/**
+ * DataStore Interface needed for parsing and instantiating products and users
+ *
+ * A derived version of the DataStore can provide other services as well but
+ * must support those below
+ *
+ */
+
+
+class MyDataStore : DataStore {
+
+};
+
+#endif
