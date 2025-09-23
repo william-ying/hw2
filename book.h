8f980440fdc7acafbad6010cb498675021ad38ef
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 12:27:23 -0700

    Update book.h

--- book.h
+++ book.h
@@ -8,7 +8,7 @@
 #include "product.h"
 #include "util.h"
 
-class Book : public product {
+class Book : public Product {
 public:
 
   Book(const std::string name, double price, int qty, const std::string isbn, const std::string author);
