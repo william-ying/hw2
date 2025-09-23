83a98a115974033579dfa5f73d9f2157be6ddf62
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 12:20:00 -0700

    Create movie.cpp

---
+++ movie.cpp
@@ -0,0 +1,23 @@
+#include "movie.h"
+
+using namespace std;
+
+Movie::Movie(const std::string name, double price, int qty, const std::string genre, const std::string rating) :
+  Product("movie", name, price, qty),
+  genre_(genre),
+  rating_(rating)
+{}
+
+std::set<std::string> Movie::keywords() const {
+  std::set<std::string> ret1 = parseStringToWords(name_);
+  ret1.insert(genre_);
+  return ret1;
+}
+
+std::string Movie::displayString() const {
+  return (name_ + "\nGenre: " + genre_ + " Rating: " + rating_ + "\n" + price + " " + qty + " left.");
+}
+
+void Movie::dump(std::ostream& os) const {
+  os << this.displayString();
+}
