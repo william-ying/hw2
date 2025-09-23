d4e6b2d7afc2fea45f227a4ffbe12b1e508c7550
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 14:13:18 -0700

    Create mydatastore.cpp

---
+++ mydatastore.cpp
@@ -0,0 +1,31 @@
+#include "mydatastore.h"
+
+using namespace std;
+
+void MyDataStore::addProduct(Product* p) {
+  
+
+}
+
+/**
+ * Adds a user to the data store
+ */
+void MyDataStore::addUser(User* u) {
+
+}
+
+/**
+ * Performs a search of products whose keywords match the given "terms"
+ *  type 0 = AND search (intersection of results for each term) while
+ *  type 1 = OR search (union of results for each term)
+ */
+std::vector<Product*> MyDataStore::search(std::vector<std::string>& terms, int type) {
+
+}
+
+/**
+ * Reproduce the database file from the current Products and User values
+ */
+void MyDataStore::dump(std::ostream& ofile) {
+
+}
