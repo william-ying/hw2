5790f36d4b230c8ccb3d4316fafb8cf1638b0001
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-23 14:31:40 -0700

    Update mydatastore.h

--- mydatastore.h
+++ mydatastore.h
@@ -12,7 +12,26 @@
  */
 
 
-class MyDataStore : DataStore {
+class MyDataStore {
+public:
+  void addProduct(Product* p);
+
+    /**
+     * Adds a user to the data store
+     */
+  void addUser(User* u);
+
+    /**
+     * Performs a search of products whose keywords match the given "terms"
+     *  type 0 = AND search (intersection of results for each term) while
+     *  type 1 = OR search (union of results for each term)
+     */
+  std::vector<Product*> search(std::vector<std::string>& terms, int type);
+
+    /**
+     * Reproduce the database file from the current Products and User values
+     */
+  void dump(std::ostream& ofile);
 protected:
   std::vector<Product*> products;
   std::vector<User*> users;
