7fd08dec5d50a916f25397724b865d783222cb99
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-25 13:58:21 -0700

    Update clothing.h

--- clothing.h
+++ clothing.h
@@ -12,6 +12,8 @@ class Clothing : public Product {
 public:
 
   Clothing(const std::string name, double price, int qty, const std::string size, const std::string brand);
+  ~Clothing();
+
   std::set<std::string> keywords() const;
 
   std::string displayString() const;
