b9bac8d46817e6afdfda0f11e37796be57845116
William Wang <61264396+william-ying@users.noreply.github.com>
2025-09-25 22:12:04 -0700

    Update mydatastore.cpp

--- mydatastore.cpp
+++ mydatastore.cpp
@@ -47,9 +47,11 @@ std::vector<Product*> MyDataStore::search(std::vector<std::string>& terms, int t
         for (Product* p : products) {
           if (p->keywords().count(temp) == 1) {
             comparator.insert(p);
+            cout << "found" << endl;
           }
         }
       }
+      cout << "or" << endl;
       pairings[temp] = comparator;
       base = setUnion(base, pairings[temp]);
     }
